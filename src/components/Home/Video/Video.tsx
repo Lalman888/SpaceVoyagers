7d403d32282319d47890bbdacd03cfc8e6977a2d
Lalman <chauhanlakshay074@gmail.com>
2023-05-18 21:20:45 +0530

    fix: bg color change

--- src/components/Home/Video/Video.tsx
+++ src/components/Home/Video/Video.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Video = () => {
   return (
     <>
-    <section className="py-16">
+    <section className="py-16 bg-black/80">
         <div className="container mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {[1, 2, 3].map((item) => (
